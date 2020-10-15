3c55db938495ae54ab9ba4a2927bd41ed509dcbc
David Jones <coreldavid@hotmail.com>
2020-10-15 19:34:18 -0400

    New Java Challenge

---
+++ src/com/company/WelcomeToJava/WelcomeToJava.java
@@ -0,0 +1,12 @@
+package com.company.WelcomeToJava;
+
+public class WelcomeToJava {
+    public WelcomeToJava(){
+
+    }
+
+    public void Display(){
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
